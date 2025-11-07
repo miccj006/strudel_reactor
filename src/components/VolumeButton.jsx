6974b320b30fc84df65bc59a44bc60fe1b7c39bd
miccj006 <miccj006@mymail.unisa.edu.au>
2025-11-08 02:06:13 +1030

    Move custom css to index.css and adjust colour of volume sliders

--- src/components/VolumeButton.jsx
+++ src/components/VolumeButton.jsx
@@ -1,7 +1,7 @@
 import VolumeIcon from '../assets/VolumeIcon'
 function VolumeButton({ volume, setMute, isMute }) {
     return (
-        <button className="btn btn-white col-1 w-auto h-auto bg-white rounded shadow-sm" id="master-volume-button" onClick={(e) => setMute(!isMute)} >
+        <button className="btn btn-white col-1 w-auto h-auto bg-light rounded shadow-sm" id="master-volume-button" onClick={(e) => setMute(!isMute)} >
             <VolumeIcon volume={volume} maxVolume={1} isMute={isMute} />
         </button>
     );
--- src/components/VolumeSliderControl.jsx
+++ src/components/VolumeSliderControl.jsx
@@ -2,7 +2,7 @@ import VolumeButton from './VolumeButton'
 function VolumeControl({ volume, setMute, isMute, onVolumeChange, setProcessSong }) {
     return (
         <>
-            <input type="range" className="form-range col p-3 h-auto bg-white rounded shadow-sm" min="0" max="1" step="0.01" id="volume-range" value={volume} onChange={(e) => onVolumeChange(e.target.value)} onMouseUp={setProcessSong} />
+            <input type="range" className="form-range col p-3 h-auto bg-light rounded shadow-sm" min="0" max="1" step="0.01" id="volume-range" value={volume} onChange={(e) => onVolumeChange(e.target.value)} onMouseUp={setProcessSong} />
             <VolumeButton volume={volume} setMute={setMute} isMute={isMute} />
         </>
     );
