f45c2487ab18c518ec979973373341d82ee48f90
miccj006 <miccj006@mymail.unisa.edu.au>
2025-11-08 01:41:02 +1030

    Fixed cycle field to merge naturally with itself

--- src/components/CycleField.jsx
+++ src/components/CycleField.jsx
@@ -4,16 +4,10 @@
 
     return (
         <>
-            <div className="input-group-prepstart">
-                <button className="btn  btn-outline-primary input-group-text" id="cycle-label" onClick={(e) => setSongText(setCycleInterval(songText, cycleData))}>Set {cycleData.isPerMinute ? "CPM" : "CPS"} ⇅</button>
-            </div>
+            <button className="btn btn-outline-primary input-group-text" id="cycle-label" onClick={(e) => setSongText(setCycleInterval(songText, cycleData))}>Set {cycleData.isPerMinute ? "CPM" : "CPS"} ⇅</button>
             <input type="text" className="form-control" id="cycle-text-input" placeholder="Insert cycle value here" aria-label="cycle" aria-describedby="cycle-label" value={cycleData.cycleText} onChange={(e) => setSongText(setCycleValue(songText, cycleData, e.target.value))} />
-            <div className="input-group-prepend">
-                <span className="input-group-text text-muted" id="cycle-label">CPS = {cpsValue}</span>
-            </div>
-            <div className="input-group-prepend">
-                <span className="input-group-text text-muted" id="cycle-label">CPM = {cpmValue}</span>
-            </div>
+            <span className="input-group-text text-muted" id="cycle-label">CPS = {cpsValue}</span>
+            <span className="input-group-text text-muted" id="cycle-label">CPM = {cpmValue}</span>
         </>
     );
 }
