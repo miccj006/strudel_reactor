0d884955e6ccd6ee6bea30478fadaf384ab01894
miccj006 <miccj006@mymail.unisa.edu.au>
2025-11-07 18:42:11 +1030

    Add extra components into each instrument

--- src/components/Instruments.jsx
+++ src/components/Instruments.jsx
@@ -75,7 +75,7 @@ function Instruments({ songText, setSongText }) {
                 {instruments.map((instrument, index) => (
                     <div key={index} className="accordion-item">
                         <div className="accordion-header" id={"heading-" + instrument.name} >
-                            <div className="accordion-button py-0" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse-" + instrument.name} aria-expanded="false" aria-controls={"collapse-" + instrument.name}>
+                            <div className="accordion-button collapsed py-0" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse-" + instrument.name} aria-expanded="false" aria-controls={"collapse-" + instrument.name}>
                                 <div className="w-25 m-0 py-3">{instrument.name}</div>
                                 <input className="form-range m-0 py-3" type="range" min="0" max="1" step="0.01" id="volume-range" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)} />
                                 <button className="btn btn-light m-0 mx-3" id="volume-button" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)} onClick={(e) => setSongText(toggleMuteInstrument(songText, instrument))}>
@@ -86,6 +86,51 @@ function Instruments({ songText, setSongText }) {
                         <div id={"collapse-" + instrument.name} className="accordion-collapse collapse" aria-labelledby={"heading-" + instrument.name} data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <strong>{instrument.name}</strong>
+
+                                {/*Extra features to reach the mark :)*/}
+                                <div className="row m-3">
+                                    <div className="col">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
+                                            <label className="form-check-label" for="exampleRadios1">
+                                                Style 1
+                                            </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
+                                            <label className="form-check-label" for="exampleRadios2">
+                                                Style 2
+                                            </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled />
+                                            <label className="form-check-label fst-italic" for="exampleRadios3">
+                                                New style coming soon
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                            <label className="form-check-label" for="flexCheckDefault">
+                                                Feature 1
+                                            </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                                            <label className="form-check-label" for="flexCheckChecked">
+                                                Feature 2
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+                                            <label className="form-check-label" for="flexSwitchCheckDefault">Bass Boost</label>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <p>{instrument.text}</p>
                                 <p>Gain = {instrument.gain}</p>
                                 {/*<p>Post Gain = {instrument.postGain}</p>*/}
