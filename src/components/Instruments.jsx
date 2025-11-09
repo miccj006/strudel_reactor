ea83a984eed756f2f8976aaef7139b5355734c4a
miccj006 <miccj006@mymail.unisa.edu.au>
2025-11-09 15:36:46 +1030

    Adjust volume slider to work nicer with instrument gain section

--- src/components/Instruments.jsx
+++ src/components/Instruments.jsx
@@ -51,61 +51,63 @@ function Instruments({ songText, setSongText }) {
                     <div key={index} className="accordion-item shadow-sm">
                         <div className="accordion-header" id={"heading-" + instrument.name} >
                             <div className="accordion-button collapsed py-0" type="button" data-bs-toggle="collapse" data-bs-target={"#collapse-" + instrument.name} aria-expanded="false" aria-controls={"collapse-" + instrument.name}>
-                                <div className="col-2 m-0 py-3">{instrument.name}</div>
+                                <div className="col-2 m-0 py-3 h6"><b>{instrument.name}</b></div>
                                 <div className='mx-1'></div>
-                                {/*<input className="form-range m-0 py-3" type="range" min="0" max="1" step="0.01" id="volume-range" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)} />*/}
-                                {/*<button className="btn btn-light m-0 mx-3" id="volume-button" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)} onClick={(e) => setSongText(toggleMuteInstrument(songText, instrument))}>*/}
-                                {/*    <VolumeIcon volume={1} maxVolume={1} isMute={instrument.mute} />*/}
-                                {/*</button>*/}
-                                <div className="row col mx-3 align-content-center" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)}>
-                                    {instrument.gains.map((gain, gainIndex) => (
-                                        <VolumeSliderControl volume={gain} maxVolume={2} setMute={(e) => setSongText(toggleMuteInstrument(songText, instrument))} isMute={instrument.mute}
-                                            onVolumeChange={(newGain) => setSongText(setInstrumentGain(songText, instrument, gainIndex, newGain))} setProcessSong={(e) => ''} />
-                                    ))}
-                                </div>
+                                {/*<div className="row col mx-3 align-content-center" onMouseEnter={(e) => DisableAccordion(e)} onMouseLeave={(e) => EnableAccordion(e)}>*/}
+                                {/*    {instrument.gains.map((gain, gainIndex) => (*/}
+                                {/*        <VolumeSliderControl volume={gain} maxVolume={2} setMute={(e) => setSongText(toggleMuteInstrument(songText, instrument))} isMute={instrument.mute}*/}
+                                {/*            onVolumeChange={(newGain) => setSongText(setInstrumentGain(songText, instrument, gainIndex, newGain))} setProcessSong={(e) => ''} />*/}
+                                {/*    ))}*/}
+                                {/*</div>*/}
                                 <div className='mx-1'></div>
                             </div>
                         </div>
-                        <div id={"collapse-" + instrument.name} className="accordion-collapse collapse" aria-labelledby={"heading-" + instrument.name} data-bs-parent="#accordionExample">
+                        <div id={"collapse-" + instrument.name} className="accordion-collapse collapse bg-light" aria-labelledby={"heading-" + instrument.name} data-bs-parent="#accordionExample">
                             <div className="accordion-body">
-                                <strong>{instrument.name}</strong>
-
-                                {/*Extra features to reach the mark :)*/}
-                                <div className="row m-3">
-                                    <div className="col">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios1" + index} value="option1" defaultChecked />
-                                            <label className="form-check-label" for={"exampleRadios1" + index}>
-                                                Style 1
-                                            </label>
+                                <div className="row">
+                                    <div className="col m-0">
+                                        <h6 className="p-0">Gain sliders</h6>
+                                            {instrument.gains.map((gain, gainIndex) => (
+                                                <div className="row my-2 align-content-center gap-2 m-0">
+                                                    <VolumeSliderControl volume={gain} maxVolume={2} setMute={(e) => setSongText(toggleMuteInstrument(songText, instrument))} isMute={instrument.mute}
+                                                        onVolumeChange={(newGain) => setSongText(setInstrumentGain(songText, instrument, gainIndex, newGain))} setProcessSong={(e) => ''} />
+                                                </div>
+                                            ))}
                                         </div>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios2" + index} value="option2" />
-                                            <label className="form-check-label" for={"exampleRadios2" + index}>
-                                                Style 2
-                                            </label>
-                                        </div>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios3" + index} value="option3" disabled />
-                                            <label className="form-check-label fst-italic" for={"exampleRadios3" + index}>
-                                                New style coming soon
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div className="col">
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                                            <label className="form-check-label" for="flexCheckDefault">
-                                                Feature 1
-                                            </label>
-                                        </div>
-                                        <div className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
-                                            <label className="form-check-label" for="flexCheckChecked">
-                                                Feature 2
-                                            </label>
-                                        </div>
-                                    </div>
+                                    {/*<div className="col">*/}
+                                    {/*    <div className="form-check">*/}
+                                    {/*        <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios1" + index} value="option1" defaultChecked />*/}
+                                    {/*        <label className="form-check-label" for={"exampleRadios1" + index}>*/}
+                                    {/*            Style 1*/}
+                                    {/*        </label>*/}
+                                    {/*    </div>*/}
+                                    {/*    <div className="form-check">*/}
+                                    {/*        <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios2" + index} value="option2" />*/}
+                                    {/*        <label className="form-check-label" for={"exampleRadios2" + index}>*/}
+                                    {/*            Style 2*/}
+                                    {/*        </label>*/}
+                                    {/*    </div>*/}
+                                    {/*    <div className="form-check">*/}
+                                    {/*        <input className="form-check-input" type="radio" name={"exampleRadios" + index} id={"exampleRadios3" + index} value="option3" disabled />*/}
+                                    {/*        <label className="form-check-label fst-italic" for={"exampleRadios3" + index}>*/}
+                                    {/*            New style coming soon*/}
+                                    {/*        </label>*/}
+                                    {/*    </div>*/}
+                                    {/*</div>*/}
+                                    {/*<div className="col">*/}
+                                    {/*    <div className="form-check">*/}
+                                    {/*        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />*/}
+                                    {/*        <label className="form-check-label" for="flexCheckDefault">*/}
+                                    {/*            Feature 1*/}
+                                    {/*        </label>*/}
+                                    {/*    </div>*/}
+                                    {/*    <div className="form-check">*/}
+                                    {/*        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />*/}
+                                    {/*        <label className="form-check-label" for="flexCheckChecked">*/}
+                                    {/*            Feature 2*/}
+                                    {/*        </label>*/}
+                                    {/*    </div>*/}
+                                    {/*</div>*/}
                                     <div className="col">
                                         <div className="form-check form-switch">
                                             <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
--- src/components/VolumeSliderControl.jsx
+++ src/components/VolumeSliderControl.jsx
@@ -2,7 +2,7 @@ import VolumeButton from './VolumeButton'
 function VolumeControl({ volume, maxVolume, setMute, isMute, onVolumeChange, setProcessSong }) {
     return (
         <>
-            <input type="range" className="form-range col p-3 h-auto bg-white rounded shadow-sm" min="0" max={maxVolume} step="0.01" id="volume-range" value={volume} onChange={(e) => onVolumeChange(e.target.value)} onMouseUp={setProcessSong} />
+            <input type="range" className="form-range col px-3 py-2 h-auto bg-white rounded shadow-sm" min="0" max={maxVolume} step="0.01" id="volume-range" value={volume} onChange={(e) => onVolumeChange(e.target.value)} onMouseUp={setProcessSong} />
             <VolumeButton volume={volume} maxVolume={maxVolume} setMute={setMute} isMute={isMute} />
         </>
     );
